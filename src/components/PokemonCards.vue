7302ce9a2eb5369dafcea4e5dba73d49779c6e7a
Aayush Batra <aayushbatra@innotechteam.in>
2024-05-04 11:53:21 +0530

    add loader

--- src/components/PokemonCards.vue
+++ src/components/PokemonCards.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="grid grid-cols-3 gap-4 mt-10 w-[50rem] m-auto">
-    <div
-      @click="
-        () => {
-          if (pokemon.evolved) {
-            console.log('Pokemon already evolved');
-          } else {
-            $emit('showEvolutionHandler', pokemon.id);
+  <div class="w-[15rem] sm:w-[30rem] lg:w-[50rem] m-auto">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-4 2xl:gap-6 mt-10 2xl:mt-16">
+      <div
+        :class="{ 'cursor-pointer': !pokemon.evolved }"
+        @click="
+          () => {
+            if (!pokemon.evolved) {
+              $emit('showEvolutionHandler', pokemon.id);
+            }
           }
-        }
-      "
-      v-for="pokemon in pokemons"
-      :key="pokemon.id"
-    >
-      <PokemonCard :pokemon="pokemon">
-        <template #title>
-          <span>{{ pokemon.name }} #{{ pokemon.id }}</span>
-        </template>
-        <template #content>
-          <img :src="pokemon.image" :alt="`${pokemon.name} image`" />
-        </template>
-        <template #footer>
-          <div v-for="(type, id) in pokemon.types" :key="id">
-            {{ type }}
-          </div>
-        </template>
-      </PokemonCard>
+        "
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+      >
+        <PokemonCard :pokemon="pokemon">
+          <template #title>
+            <span>{{ pokemon.name }} #{{ pokemon.id }}</span>
+          </template>
+          <template #content>
+            <img :src="pokemon.image" :alt="`${pokemon.name} image`" />
+          </template>
+          <template #footer>
+            <div v-for="(type, id) in pokemon.types" :key="id">
+              {{ type }}
+            </div>
+          </template>
+        </PokemonCard>
+      </div>
+    </div>
+    <div class="mt-20 flex justify-center" v-if="evolutionLoading">
+      <LoadingSpinner />
     </div>
   </div>
 </template>
@@ -33,8 +37,10 @@
 <script setup>
 import { defineProps } from "vue";
 import PokemonCard from "./PokemonCard.vue";
+import LoadingSpinner from "./LoadingSpinner.vue";
 
 defineProps({
   pokemons: Array,
+  evolutionLoading: Boolean,
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
+    <div
+      v-if="mainLoading"
+      class="fixed inset-0 flex justify-center items-center"
+    >
+      <LoadingSpinner />
+    </div>
     <PokemonCards
+      v-else
       :pokemons="pokemons"
+      :evolutionLoading="evolutionLoading"
       @showEvolutionHandler="showEvolutionHandler"
     />
   </div>
 </template>
 
 <script setup>
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
 import PokemonCards from "@/components/PokemonCards.vue";
+
 import { onMounted, ref } from "vue";
 
 const pokemons = ref([]);
+let mainLoading = ref(false);
+let evolutionLoading = ref(false);
 
 const fetchPokemon = async (url) => {
   try {
@@ -35,19 +47,27 @@ const fetchPokemon = async (url) => {
 const showEvolutionHandler = async (id) => {
   const mainPokemons = pokemons.value.filter((pokemon) => !pokemon.evolved);
   pokemons.value = mainPokemons;
-  const pokemonEvolved1 = await fetchPokemon(
-    `https://pokeapi.co/api/v2/pokemon/${id + 1}`
-  );
-  const pokemonEvolved2 = await fetchPokemon(
-    `https://pokeapi.co/api/v2/pokemon/${id + 2}`
-  );
-  pokemonEvolved1.evolved = true;
-  pokemonEvolved2.evolved = true;
-  pokemons.value.push(pokemonEvolved1, pokemonEvolved2);
+  try {
+    evolutionLoading.value = true;
+    const responses = await Promise.all([
+      fetchPokemon(`https://pokeapi.co/api/v2/pokemon/${id + 1}`),
+      fetchPokemon(`https://pokeapi.co/api/v2/pokemon/${id + 2}`),
+    ]);
+    const evolvedPokemons = responses.map((pokemon) => {
+      pokemon.evolved = true;
+      return pokemon;
+    });
+    pokemons.value.push(...evolvedPokemons);
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    evolutionLoading.value = false;
+  }
 };
 
 onMounted(async () => {
   try {
+    mainLoading.value = true;
     const responses = await Promise.all([
       fetchPokemon("https://pokeapi.co/api/v2/pokemon/1"),
       fetchPokemon("https://pokeapi.co/api/v2/pokemon/4"),
@@ -56,6 +76,8 @@ onMounted(async () => {
     pokemons.value = responses;
   } catch (error) {
     console.error("Error:", error);
+  } finally {
+    mainLoading.value = false;
   }
 });
 </script>
